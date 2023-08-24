1d818373edf62239694ca932bfafdaa27b679b87
hariharaedu91 <harihara.edu91@gmail.com>
2023-08-23 20:00:54 -0500

    Home page phase one
    Signed-off-by: hariharaedu91 <harihara.edu91@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgmaterialModule } from './ng-material/ngmaterial.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { GotetiFormsModule, GtFormsService } from 'goteti-forms';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgmaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    GotetiFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+    constructor(public gtFormService: GtFormsService){
+
+    }
+ }
---
+++ src/app/ng-material/ng-mat-templates/ng-mat-templates.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-mat-templates',
+  templateUrl: './ng-mat-templates.component.html',
+  styleUrls: ['./ng-mat-templates.component.scss']
+})
+export class NgMatTemplatesComponent {
+    
+}
